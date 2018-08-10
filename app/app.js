36a38c02063e52a529284e9acf7b7a543a64e7bd
ubarum004 <a538642@hotmail.com>
2018-08-10 15:12:49 -0700

    structuring content

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var reload = require('reload');
+
 var app = express();
 var server = http.createServer(app);
 
@@ -9,7 +10,11 @@ var port = 3000;
 server.listen(process.env.PORT || port);
 
 app.set('port', process.env.PORT || port);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
-app.use(express.static('./public'));
+app.use(express.static('app/public'));
+app.use(require('./routes/index'));
+app.use(require('./routes/about'));
 
 reload(app);
---
+++ app/routes/about.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/about', function(req, res) {
+  res.render('about', {
+    pageTitle: 'About',
+    pageID: 'about'
+  });
+
+});
+
+module.exports = router;
