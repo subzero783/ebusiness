cab2a9a50d8e3637c5b151b40af66b9ef1eff991
ubarum004 <a538642@hotmail.com>
2018-08-10 17:01:35 -0700

    working on contact form

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var email = require('emailjs');
+
+//Make a secure file for sending email
+var server = email.server.connect({
+	user: "ADD USERNAME",
+	password: "ADD PASSWORD",
+	host: "smtp.gmail.com",
+	port: 465,
+	ssl: true
+});
 
 router.get('/contact', function(req, res) {
   res.render('contact', {
@@ -8,5 +18,13 @@ router.get('/contact', function(req, res) {
   });
 
 });
+
+server.send({
+   text:    "i hope this works", 
+   from:    "you <gusta9753@gmail.com>", 
+   to:      "someone <gusta9753@gmail.com>",
+   cc:      "",
+   subject: "testing emailjs"
+}, function(err, message) { console.log(err || message); });
 
 module.exports = router;
