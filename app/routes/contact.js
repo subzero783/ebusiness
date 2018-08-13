ff19cdcc1a04b382cf219bb6ce84d716cb9f8c1d
ubarum004 <a538642@hotmail.com>
2018-08-13 15:01:50 -0700

    encrypted

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var nodeMailer = require('nodemailer');
-var config = require('../../config.json'); 
+var config = require('../../config.json.cast5'); 
 
 router.use(bodyParser.urlencoded({extended : false}));
 router.use(bodyParser.json());
